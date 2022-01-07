e44ca62fecbd26ed27140316faeedc75fd3888e8
Zhark10 <verygoodman18@yandex.ru>
2022-01-08 00:15:30 +0300

    lockdown gen

---
+++ scripts/lockdown-generation.js
@@ -0,0 +1,12 @@
+import fs from "fs"
+var p = JSON.parse(fs.readFileSync('package.json'))
+var v = JSON.parse(fs.readFileSync('npm-shrinkwrap.json'))
+
+updateDependencies(p.dependencies, v.dependencies)
+updateDependencies(p.devDependencies, v.dependencies)
+
+fs.writeFileSync('package-lockdown.json', JSON.stringify(p, null, 2))
+
+function updateDependencies(list, v) {
+  for (var d in list) list[d] = v[d].version
+}
