b6d7157957a48552c97782b0e522a3e3be0e992c
Zhark10 <verygoodman18@yandex.ru>
2022-01-08 00:15:50 +0300

    replaced tokens

--- src/config/bot-initialize.js
+++ src/config/bot-initialize.js
@@ -2,4 +2,4 @@ import TelegramApi from "node-telegram-bot-api";
 
 import { CONSTANTS } from "../config/constants.js"
 
-export const bot = new TelegramApi(CONSTANTS.APIS.tgbot, { polling: true })
+export const bot = new TelegramApi(CONSTANTS.TOKENS.tgbot, { polling: true })
--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -4,7 +4,7 @@ import { CONSTANTS } from "../config/constants.js"
 
 const createDatabaseConnection = async () => {
   try {
-    await mongoose.connect(CONSTANTS.APIS.mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(CONSTANTS.TOKENS.mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Connection is successful')
   } catch (error) {
     console.error('Connection error', error)
