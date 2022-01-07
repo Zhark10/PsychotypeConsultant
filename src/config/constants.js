574cda1786c9ee4f77684a730021ceda43d91949
Zhark10 <verygoodman18@yandex.ru>
2022-01-07 05:18:47 +0300

    command constants

---
+++ src/config/constants.js
@@ -0,0 +1,16 @@
+const SEPARATOR_TO_CREATE_UNIQUE_COMMAND = "__"
+
+const COMMON_COMMANDS = {
+  sayHello: "/start",
+  runTest: "/runTest",
+}
+
+const ADMIN_COMMANDS = {
+  getStatsByCandidates: "/statsByCandidates",
+}
+
+export const CONSTANTS = {
+  SEPARATOR_TO_CREATE_UNIQUE_COMMAND,
+  COMMON_COMMANDS,
+  ADMIN_COMMANDS,
+}
