cd3d3294e7ea845fb2e58b2e1e050bf91761a396
Zhark10 <verygoodman18@yandex.ru>
2022-01-08 00:16:37 +0300

    upd command service

--- src/services/command/AdminCommandService.js
+++ src/services/command/AdminCommandService.js
@@ -1,5 +1,10 @@
 import { CommandService } from "./CommandService.js"
+import { User } from '../../models/User.js'
 
 export class AdminCommandService extends CommandService {
-  getStatsByCandidates = async (msg) => { }
+  getStatsByCandidates = async (msg) => { 
+    const allUsers = await User.find({}).exec()
+
+    console.log('allUsers', allUsers)
+  }
 }
--- src/services/command/CommandService.js
+++ src/services/command/CommandService.js
@@ -1,7 +1,8 @@
 import pkg from 'node-emoji';
 
-import { User } from '../models/User.js'
+import { User } from '../../models/User.js'
 import { getMenuCommands } from '../../utils/get-menu-commands.js'
+import { CONSTANTS } from '../../config/constants.js'
 
 const { emoji } = pkg;
 
@@ -11,18 +12,19 @@ export class CommandService {
   }
 
   sayHello = async (msg) => {
+    const chatId = msg.chat.id
+    
+    const user = await User.findOne({ firstname: msg.from.first_name, lastname: msg.from.last_name, nickname: msg.from.username }).exec();
+    const isAdmin = (user?.role === "admin")
     const menuCommands = getMenuCommands(isAdmin)
     this.bot.setMyCommands(menuCommands)
 
-    const chatId = msg.chat.id
-    const isUserAlreadyExist = await User.findOne({ firstname: msg.from.first_name, lastname: msg.from.last_name }).exec();
-    if (!isUserAlreadyExist) {
-      // Права админа задаются в базе вручную при необходимости привязки этой роли к конкретному юзеру
-      await User.create({ firstname: msg.from.first_name, lastname: msg.from.last_name, nickname: msg.from.login, role: 'candidate' })
+    if (!user) {
+      await User.create({ firstname: msg.from.first_name, lastname: msg.from.last_name, nickname: msg.from.username, role: 'candidate' })
     }
 
     await this.bot.sendMessage(chatId, `Привет, ${msg.from.first_name} ${msg.from.last_name}${emoji.v} Я хочу задать тебе несколько вопросов, чтобы определить твой психотип. Давай начнем!`)
-    return this.bot.sendMessage(chatId, 'Для начала нажми на /runTest')
+    return this.bot.sendMessage(chatId, `Для начала нажми на ${CONSTANTS.COMMON_COMMANDS.runTest}`)
   }
 
   runTest = async (msg) => { }
