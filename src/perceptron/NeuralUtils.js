822d228133faffb13413f06e5368361e8fe7c292
Zhark10 <verygoodman18@yandex.ru>
2022-01-08 14:38:05 +0300

    add neural utils

---
+++ src/perceptron/NeuralUtils.js
@@ -0,0 +1,14 @@
+export class NeuralUtils {
+  constructor() { }
+
+  sigmoid(t) {
+    return 1 / (1 + Math.exp(-z));
+  }
+
+  get rates() {
+    return {
+      IS_THE_DESIRED_CANDIDATE: 1,
+      IS_NOT_THE_DESIRED_CANDIDATE: 0
+    }
+  }
+}
