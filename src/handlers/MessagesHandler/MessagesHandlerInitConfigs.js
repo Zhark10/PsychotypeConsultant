a30fc2c2d58c860fd2ff6fc8d1182e36b3ec1707
Zhark10 <verygoodman18@yandex.ru>
2022-01-14 00:25:02 +0300

    create init configs for messages handler

---
+++ src/handlers/MessagesHandler/MessagesHandlerInitConfigs.js
@@ -0,0 +1,25 @@
+import { User } from '../../models/User.js'
+import { CONSTANTS } from "../../config/constants.js"
+import { getCommonUserInfo } from '../../utils/get-common-user-info.js'
+
+import { CandidateCommandService } from "../../services/command/CandidateCommandService.js"
+import { EmployeeCommandService } from "../../services/command/EmployeeCommandService.js"
+import { AdminCommandService } from "../../services/command/AdminCommandService.js"
+import { CommandService } from "../../services/command/CommandService.js"
+
+export const initConfigsForMessagesHandler = async (msg, dependencies) => {
+  const user = await User.findOne(getCommonUserInfo(msg)).exec();
+  const role = user?.role;
+
+  const services = {
+    [CONSTANTS.USER_ROLES.ADMIN]: AdminCommandService,
+    [CONSTANTS.USER_ROLES.EMPLOYEE]: EmployeeCommandService,
+    [CONSTANTS.USER_ROLES.CANDIDATE]: CandidateCommandService
+  }
+
+  const Service = services[role] || CommandService
+
+  const definedService = new Service(dependencies)
+
+  return { definedService }
+}
