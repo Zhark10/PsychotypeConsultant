87e0ccd012987dc1c75bf7277faf898f5187e721
Zhark10 <verygoodman18@yandex.ru>
2022-01-13 04:07:55 +0300

    fix get common info

--- src/services/command/helpers/get-info-for-actions.js
+++ src/services/command/helpers/get-info-for-actions.js
@@ -1,7 +1,8 @@
 import { User } from '../../../models/User.js'
+import { getCommonUserInfo } from '../../../utils/get-common-user-info.js'
 
 export const getCommonInfoForActions = async (msg) => {
   const user = await User.findOne(getCommonUserInfo(msg)).exec();
-  const chatId = this.msg.chat.id
+  const chatId = msg.chat.id
   return { user, chatId }
 }
