f6b9ffd9fa9d04d43370a002cd57a9757c4e299e
Zhark10 <verygoodman18@yandex.ru>
2022-01-14 00:24:21 +0300

    ask a question method in candidate command service

--- src/services/command/CandidateCommandService.js
+++ src/services/command/CandidateCommandService.js
@@ -1,3 +1,8 @@
 import { CommandService } from "./CommandService.js"
+import {CONSTANTS} from "../../config/constants.js"
 
-export class CandidateCommandService extends CommandService { }
+export class CandidateCommandService extends CommandService {
+  [CONSTANTS.COMMANDS.ASK_A_QUESTION] = async () => {
+    const { user, chatId } = await getCommonInfoForActions(this.msg)
+  }
+ }
