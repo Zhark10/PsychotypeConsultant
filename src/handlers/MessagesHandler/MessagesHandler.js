d98496011d996f4255bf792534dd4e4494af37b8
Zhark10 <verygoodman18@yandex.ru>
2022-01-16 04:44:02 +0300

    add callback query

--- src/handlers/MessagesHandler/MessagesHandler.js
+++ src/handlers/MessagesHandler/MessagesHandler.js
@@ -1,18 +1,41 @@
-import { NeuralNetwork } from '../../perceptron/NeuralNetwork.js'
-import { initConfigsForMessagesHandler } from './MessagesHandlerInitConfigs.js'
+import { NeuralNetwork } from "../../perceptron/NeuralNetwork.js"
+import { initConfigsForMessagesHandler } from "./MessagesHandlerInitConfigs.js"
 import { CONSTANTS } from "../../config/constants.js"
+import { User } from '../../models/User.js'
 
 export const MessagesHandler = async (bot) => {
-  let neuralNetwork;
-
+  let definedService;
+  const neuralNetwork = new NeuralNetwork();
+  
   bot.on("message", async (msg) => {
-    neuralNetwork = new NeuralNetwork()
     const dependencies = { bot, msg, neuralNetwork }
+    definedService = await initConfigsForMessagesHandler(msg, dependencies)
 
-    const configs = await initConfigsForMessagesHandler(msg, dependencies)
-    const { definedService } = configs
-
-    const action = definedService[msg.text] || service[CONSTANTS.COMMANDS.THROW_FALLBACK_MESSAGE]
+    const action = definedService[msg.text] || definedService[CONSTANTS.COMMANDS.THROW_FALLBACK_MESSAGE]
     return action()
   })
+
+  bot.on("callback_query", async (callbackQuery) => {
+    const msg = callbackQuery.message;
+    const userDataToSearch = { firstname: msg.chat.first_name, lastname: msg.chat.last_name, nickname: msg.chat.username }
+    const user = await User.findOne(userDataToSearch).exec();
+
+    const question = {
+      currentQuestionIndex: user.testAnswers?.length,
+      nextQuestionIndex: user.testAnswers?.length + 1,
+    }
+
+    const answerId = callbackQuery.data;
+    await User.updateOne(userDataToSearch, { $push: { testAnswers: answerId } }).exec();
+
+    console.log(question.currentQuestionIndex, answerId)
+    
+    
+    if (!question.nextQuestionIndex) {
+      bot.sendMessage('Тест завершен! Спасибо за участие!')
+    }
+    
+    const action = definedService[CONSTANTS.COMMANDS.ASK_NEXT_QUESTION]
+    return action(question.nextQuestionIndex)
+  });
 }
