9d7c2a67672b1eb73f5e4f3bada5c1c08ccef8d8
Zhark10 <verygoodman18@yandex.ru>
2022-01-16 04:46:02 +0300

    logic in candidate command service

--- src/services/command/CandidateCommandService.js
+++ src/services/command/CandidateCommandService.js
@@ -1,8 +1,23 @@
 import { CommandService } from "./CommandService.js"
-import {CONSTANTS} from "../../config/constants.js"
+import { CONSTANTS } from "../../config/constants.js"
+import { getCommonInfoForActions } from "./helpers/get-info-for-actions.js"
+import { getCommonUserInfo } from '../../utils/get-common-user-info.js'
+import { PsychotypeData } from "../../perceptron/PsychotypeTest.js"
+import { User } from "../../models/User.js"
 
 export class CandidateCommandService extends CommandService {
-  [CONSTANTS.COMMANDS.ASK_A_QUESTION] = async () => {
-    const { user, chatId } = await getCommonInfoForActions(this.msg)
+  [CONSTANTS.COMMANDS.RUN_TEST] = async () => {
+    const { test } = PsychotypeData
+    const firstTestItem = test[0]
+    const { chatId } = await getCommonInfoForActions(this.msg)
+    await User.updateOne(getCommonUserInfo(this.msg), { testAnswers: [] }).exec();
+    return this.bot.sendMessage(chatId, firstTestItem.question, firstTestItem.options)
+  }
+
+  [CONSTANTS.COMMANDS.ASK_NEXT_QUESTION] = async (nextQuestionIndex) => {
+    const { test } = PsychotypeData
+    const nextQuestion = test[nextQuestionIndex]
+    const { chatId } = await getCommonInfoForActions(this.msg)
+    return this.bot.sendMessage(chatId, nextQuestion.question, nextQuestion.options)
   }
  }
