9bee32c925bd2bb0181808b892ad89ebd4cc5a41
Zhark10 <verygoodman18@yandex.ru>
2022-01-19 02:23:08 +0300

    add run test method for employee

--- src/services/command/EmployeeCommandService.js
+++ src/services/command/EmployeeCommandService.js
@@ -1,8 +1,11 @@
 import { CommandService } from "./CommandService.js"
 
 export class EmployeeCommandService extends CommandService {
-  askAQuestion = async () => {
-    const { user, chatId } = await this.getCommonInfoForActions()
-    
+  [CONSTANTS.COMMANDS.RUN_TEST] = async () => {
+    const { test } = PsychotypeData
+    const firstTestItem = test[0]
+    const { chatId } = await getCommonInfoForActions(this.msg)
+    await User.updateOne(getCommonUserInfo(this.msg), { testAnswers: [] }).exec();
+    return this.bot.sendMessage(chatId, firstTestItem.question, firstTestItem.options)
   }
 }
