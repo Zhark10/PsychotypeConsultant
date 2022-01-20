1f176f67c5207d200f2f82fc06884427532a221d
Zhark10 <verygoodman18@yandex.ru>
2022-01-21 02:29:39 +0300

    upd logic of user action handler

--- src/handlers/UserActionHandler/UserActionHandler.js
+++ src/handlers/UserActionHandler/UserActionHandler.js
@@ -2,43 +2,43 @@ import { NeuralNetwork } from "../../perceptron/NeuralNetwork.js"
 import { UserActionHandlerHelpersBy } from "./UserActionHandlerHelpers.js"
 import { CONSTANTS } from "../../config/constants.js"
 import { User } from '../../models/ModelOfUser.js'
+import { PsychotypeData } from "../../perceptron/NeuralPsychotypeTest.js"
+
+const { test } = PsychotypeData
 
 export const UserActionHandler = async (bot) => {
   let definedService;
   const neuralNetwork = new NeuralNetwork();
-  
+
   bot.on("message", async (msg) => {
     const dependencies = { bot, msg, neuralNetwork }
     definedService = await UserActionHandlerHelpersBy.messages.defineService(msg, dependencies)
     const action = definedService[msg.text] || definedService[CONSTANTS.COMMANDS.THROW_FALLBACK_MESSAGE]
-    return action() 
+    return action()
   })
 
   bot.on("callback_query", async (callbackQuery) => {
     const msg = callbackQuery.message;
+    const chatId = msg.chat.id
     const userDataToSearch = { firstname: msg.chat.first_name, lastname: msg.chat.last_name, nickname: msg.chat.username }
-    const user = await User.findOne(userDataToSearch).exec();
-
-    const question = {
-      currentQuestionIndex: user.testAnswers?.length,
-      nextQuestionIndex: user.testAnswers?.length + 1,
-    }
 
     const answerId = callbackQuery.data;
-    await User.updateOne(userDataToSearch, { $push: { testAnswers: answerId } }).exec();
+    const updatedUser = await User.findOneAndUpdate(
+      userDataToSearch,
+      { $push: { testAnswers: answerId } },
+      { new: true },
+    )
 
-    if (!question.nextQuestionIndex) {
-      bot.sendMessage('Тест завершен! Спасибо за участие!')
-      const isForTrainingSet = [CONSTANTS.USER_ROLES.ADMIN, CONSTANTS.USER_ROLES.EMPLOYEE].some(user.role)
+    if (updatedUser.testAnswers.length === test.length) {
+      bot.sendMessage(chatId, 'Тест завершен! Спасибо за участие!')
+      const isForTrainingSet = [CONSTANTS.USER_ROLES.ADMIN, CONSTANTS.USER_ROLES.EMPLOYEE].includes(updatedUser.role)
       if (isForTrainingSet) {
         neuralNetwork.perceptronTrain()
-        return
       }
-      
-      // TODO: candidate score calculation
+      neuralNetwork.runTestAndSendResult(updatedUser._id)
+    } else {
+      const action = definedService[CONSTANTS.COMMANDS.ASK_NEXT_QUESTION]
+      return action(updatedUser.testAnswers.length)
     }
-    
-    const action = definedService[CONSTANTS.COMMANDS.ASK_NEXT_QUESTION]
-    return action(question.nextQuestionIndex)
   });
 }
