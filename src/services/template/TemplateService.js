d866f63f939f4f68f21830e7566b56f3f31c9bc2
Zhark10 <verygoodman18@yandex.ru>
2022-01-22 01:42:30 +0300

    create template for welcome message method

--- src/services/template/TemplateService.js
+++ src/services/template/TemplateService.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import { getHtmlTemplateForAnswer } from './helpers/get-html-template-for-answer.js'
+import { getHtmlTemplateForWelcomeMessage } from './helpers/get-html-template-for-welcome-message.js'
 
 export class TemplateService {
   constructor() { }
@@ -16,4 +17,17 @@ export class TemplateService {
     await browser.close();
     return picture;
   };
+
+  createTemplateForWelcomeMessage = async (message) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({width: 360, height: 96, deviceScaleFactor: 3});
+    await page.setContent(getHtmlTemplateForWelcomeMessage(message));
+    const picture = await page.screenshot({
+      type: 'png',
+      fullPage: true,
+    });
+    await browser.close();
+    return picture;
+  }
 }
