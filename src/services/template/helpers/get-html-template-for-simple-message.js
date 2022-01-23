ecb8a42dc34074af6c1bbab5fc0b85a728dd25d0
Zhark10 <verygoodman18@yandex.ru>
2022-01-24 00:51:04 +0300

    upd styles for second template

--- src/services/template/helpers/get-html-template-for-simple-message.js
+++ src/services/template/helpers/get-html-template-for-simple-message.js
@@ -11,6 +11,15 @@ export const getHtmlTemplateForSimpleMessage = (message) => {
       background-color: ${colors.secondary};
       font-family: Courier New;
     ">
+      <div style="
+        position: fixed;
+        height: 520px;
+        width: 520px;
+        border: 150px solid rgba(0,0,0,0.05);
+        top: -80px;
+        left: 0;
+        border-radius: 50%;
+      "></div>
       <div style=" 
         display: flex; 
         align-items: center; 
