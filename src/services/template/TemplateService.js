ced643f0a6b268e646f9f31bdc4a9d8a0252d6d4
Zhark10 <verygoodman18@yandex.ru>
2022-01-26 00:34:48 +0300

    new method in template service

--- src/services/template/TemplateService.js
+++ src/services/template/TemplateService.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import { getHtmlTemplateForAnswer } from './helpers/get-html-template-for-answer.js'
+import { getHtmlTemplateForStats } from './helpers/get-html-template-for-stats.js'
 import { getHtmlTemplateForSimpleMessage } from './helpers/get-html-template-for-simple-message.js'
 
 export class TemplateService {
@@ -30,4 +31,18 @@ export class TemplateService {
     await browser.close();
     return picture;
   }
+
+  createTemplateForStats = async (results) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const calculatedHeight = 48 + (results?.length * 18)
+    await page.setViewport({width: 360, height: calculatedHeight, deviceScaleFactor: 3});
+    await page.setContent(getHtmlTemplateForStats(results, calculatedHeight));
+    const picture = await page.screenshot({
+      type: 'png',
+      fullPage: true,
+    });
+    await browser.close();
+    return picture;
+  };
 }
