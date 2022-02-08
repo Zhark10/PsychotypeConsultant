375765d93a96520e3aa2016d3b672141e1953e8c
Zhark10 <verygoodman18@yandex.ru>
2022-02-09 01:49:35 +0300

    modified get common user info func

--- src/services/command/helpers/get-info-for-actions.js
+++ src/services/command/helpers/get-info-for-actions.js
@@ -1,8 +1,8 @@
 import { User } from '../../../models/ModelOfUser.js'
-import { getCommonUserInfo } from '../../../utils/get-common-user-info.js'
+import { getCommonUserInfoByMsg } from '../../../utils/get-common-user-info.js'
 
 export const getCommonInfoForActions = async (msg) => {
-  const user = await User.findOne(getCommonUserInfo(msg)).exec();
+  const user = await User.findOne(getCommonUserInfoByMsg(msg)).exec();
   const chatId = msg.chat.id
   return { user, chatId }
 }
--- src/utils/get-common-user-info.js
+++ src/utils/get-common-user-info.js
@@ -1,3 +1,10 @@
-export const getCommonUserInfo = (msg) => {
-  return { firstname: msg.from.first_name, lastname: msg.from.last_name, nickname: msg.from.username }
+export const getCommonUserInfoByMsg = (msg) => {
+  const isRegularMessage = Boolean(msg.from?.first_name)
+  const isCallbackQuery = Boolean(msg.chat?.first_name)
+  if (isCallbackQuery) {
+    return { firstname: msg.chat.first_name, lastname: msg.chat.last_name, nickname: msg.chat.username }
+  } else if (isRegularMessage) {
+    return { firstname: msg.from.first_name, lastname: msg.from.last_name, nickname: msg.from.username }
+  }
+  return {}
 }
